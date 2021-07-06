822f06e86159a1ab6c9f1466f10a9799c6566aed
afoote1024 <afoote1024@aol.com>
2021-07-06 09:28:39 -0700

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, Azure!  Python Django - GitHub python-docs-hello-django/hello/views.py")
+    return HttpResponse(<p>"Hello, Azure!  Python Django</p>
+                        <p>- GitHub python-docs-hello-django/hello/views.py</p>"
+                       
+                       )
